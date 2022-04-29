1ec749db1d5c496cf540e1a1369f28804e8e83db
Paul-Henri Andrieu <paul-henri@phandrieu.fr>
2022-04-29 10:54:31 +0200

    Finished cleaning up

--- huffman.ml
+++ huffman.ml
@@ -2,13 +2,16 @@ open Compress;;
 open Decompress;;
 
 let main () =
-        if Sys.argv.(1) = "comp"
-        then 
-                begin
-                        Compress.compress Sys.argv.(2);
-                end
-        else if Sys.argv.(1) = "decomp"
-        then Decompress.decompress Sys.argv.(2)
+        if Array.length Sys.argv <> 2 
+                then
+
+                if Sys.argv.(1) = "comp"
+                then 
+                        begin
+                                Compress.compress Sys.argv.(2);
+                        end
+                else if Sys.argv.(1) = "decomp"
+                then Decompress.decompress Sys.argv.(2) 
         else Printf.printf "Usage: huffman comp/decomp fichier.ext \n"
 ;;
 
