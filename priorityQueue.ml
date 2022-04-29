ccf52451fcf00cdb81cdd26dcfced73e2ff8d4ca
Paul-Henri Andrieu <paul-henri@phandrieu.fr>
2022-04-29 10:29:41 +0200

    Cleaned up

--- priorityQueue.ml
+++ priorityQueue.ml
@@ -10,13 +10,6 @@ let length file =
         Heap.length file
 ;;
 
-(*Fonction aux : comparaison de deux éléments (prio, el) pour pouvoir utiliser le tri*)
-let comparaison (prio1, el1) (prio2, el2) = 
-        if prio1 > prio2 then 1
-        else if prio1 < prio2 then -1
-        else 0  (*si égaux*)
-;;
-
 
 let add el p file = 
         Heap.add (p, el) file
