94d4fac198483d1717f7de69f473c6773e35def4
Paul-Henri Andrieu <paul-henri@phandrieu.fr>
2022-04-29 10:36:02 +0200

    Cleaned up

--- decompress.ml
+++ decompress.ml
@@ -1,20 +1,6 @@
 open FileIO;;
 open HuffmanTree;;
 
-let int_to_bool = function 0 -> false | 1 -> true | a -> print_int a;  failwith "?" ;;
-
-let bool_liste_of_int (nbr_ : int) =
-  let nbr = ref nbr_ in
-  let res = ref [] in
-
-  while !nbr <> 0 do
-    res := (int_to_bool (!nbr mod 2)) :: (!res);
-    nbr := (!nbr) / 2;
-  done;
-  !res
-;;
-
-
 let rec input_code (inp : FileIO.in_channel_bits) (arb : HuffmanTree.tree )=
         match arb with
         |Char c -> c
@@ -22,18 +8,7 @@ let rec input_code (inp : FileIO.in_channel_bits) (arb : HuffmanTree.tree )=
                 if(FileIO.input_bit inp)
                         then input_code inp r
                         else input_code inp l
-                ;;
-
-
-(*let __input_code (inp : FileIO.in_channel_bits) (arb : HuffmanTree.tree) =
-
-        let codes = HuffmanTree.codes arb in
-        let i = ref 0 in
-        while (!i < 256) && (codes.(!i) <> octets) do
-                i := !i +1;
-        done;
-        !i
-;;*)
+;;
 
 let decompress (fn : string) =
         let out = open_out_bits (String.sub fn 0 (String.length fn -8)) in
@@ -47,4 +22,3 @@ let decompress (fn : string) =
         close_in_bits inp;
         close_out_bits out
 ;;
-
